c4155a3da8d184564336700fca1ce27d7ea1bc5a
Wataru Terada <w.terada@gmail.com>
2016-02-20 10:49:29 +0900

    Excel用のテスト追加

--- test/CsvWriterControllerTest.php
+++ test/CsvWriterControllerTest.php
@@ -1,43 +1,49 @@
 <?php
 use waterada\CsvFileIterator\CsvFileIterator;
 use waterada\CsvFileIterator\RecordLimitException;
-use waterada\CsvFileIterator\WritingPosition;
-use waterada\CsvFileWriter\CsvFileOnWeb;
+use waterada\CsvFileWriter\WritingPosition;
+use waterada\CsvFileOnWeb\CsvFileOnWeb;
 use waterada\CsvFileWriter\CsvFileWriterFlow;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
-require_once __DIR__ . '/../src/CsvFileWriter/CsvFileWriter.php';
-require_once __DIR__ . '/../src/CsvFileWriter/CsvFileWriterFlow.php';
-require_once __DIR__ . '/../src/CsvFileWriter/Output.php';
-require_once __DIR__ . '/../src/CsvFileWriter/Format/Format.php';
-require_once __DIR__ . '/../src/CsvFileWriter/Format/CsvFormat.php';
-require_once __DIR__ . '/../src/CsvFileWriter/Format/XlsxFormat.php';
-require_once __DIR__ . '/../src/CsvFileWriter/WritingPosition.php';
-require_once __DIR__ . '/../src/CsvFileOnWeb/CsvFileOnWeb.php';
 
 /**
  * @property PHPUnit_Framework_MockObject_MockObject|CsvFileOnWeb $onWeb
  * @property array $session
- * @property array $data
  * @property array $actual
- * @property array $expected
+ * @property string[] paths
  */
 class CsvWriterControllerTest extends PHPUnit_Framework_TestCase
 {
     const RECORD_LIMIT = 3;
 
+    private $paths = [];
+
     public function setUp()
     {
         $this->onWeb = null;
         $this->session = [];
-        $this->data = []; //todo: ひつようか
         $this->actual = [];
-        $this->expected = [];
+        $this->paths = [];
+    }
+
+    public function tearDown()
+    {
+        foreach ($this->paths as $path) {
+            if (file_exists($path)) {
+                unlink($path);
+            }
+        }
+    }
+
+    public function __destruct()
+    {
+        $this->tearDown();
     }
 
     private function __mock_OnWeb()
     {
-        $this->onWeb = $this->getMockBuilder('waterada\CsvFileWriter\CsvFileOnWeb')->setMethods([
+        $this->onWeb = $this->getMockBuilder('waterada\CsvFileOnWeb\CsvFileOnWeb')->setMethods([
             'ajaxHeader',
             'ajaxBody',
             'downloadHeader',
@@ -47,9 +53,33 @@ class CsvWriterControllerTest extends PHPUnit_Framework_TestCase
 
     //--------------------
 
-    public function test_read()
+    public function cases_read_CSV_XLSX()
     {
-        $path = FileFabricate::fromString("ID\n1\n2\n3\n4\n5\n6\n7\n8\n")->getPath();
+        return [
+            [
+                FileFabricate::fromString("ID\n1\n2\n3\n4\n5\n6\n7\n8\n")->getPath(),
+                strlen("ID\n1\n2\n3\n4\n5\n6\n7\n8\n"),
+                strlen("ID\n1\n2\n3\n"),
+                strlen("ID\n1\n2\n3\n4\n5\n6\n"),
+            ],
+            [
+                realpath('test_suspend_8.xlsx'),
+                count(['ID']) + count(range(1, 8)),
+                count(['ID']) + count(range(1, 3)),
+                count(['ID']) + count(range(1, 6)),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider cases_read_CSV_XLSX
+     * @param string $path
+     * @param int $max
+     * @param int $cur1
+     * @param int $cur2
+     */
+    public function test_read($path, $max, $cur1, $cur2)
+    {
         $this->actual = [];
 
         //初回アクセス
@@ -57,8 +87,8 @@ class CsvWriterControllerTest extends PHPUnit_Framework_TestCase
         $this->onWeb->expects($this->once())->method('ajaxHeader');
         $this->onWeb->expects($this->once())->method('ajaxBody')->with([
             'isFinished' => false,
-            'cur' => strlen("ID\n1\n2\n3\n"),
-            'max' => filesize($path),
+            'cur' => $cur1,
+            'max' => $max,
         ]);
         $this->read($path);
         $this->assertEquals(["1", "2", "3"], $this->actual);
@@ -68,8 +98,8 @@ class CsvWriterControllerTest extends PHPUnit_Framework_TestCase
         $this->onWeb->expects($this->once())->method('ajaxHeader');
         $this->onWeb->expects($this->once())->method('ajaxBody')->with([
             'isFinished' => false,
-            'cur' => strlen("ID\n1\n2\n3\n4\n5\n6\n"),
-            'max' => filesize($path),
+            'cur' => $cur2,
+            'max' => $max,
         ]);
         $this->read($path);
         $this->assertEquals(["1", "2", "3", "4", "5", "6"], $this->actual);
@@ -110,32 +140,57 @@ class CsvWriterControllerTest extends PHPUnit_Framework_TestCase
 
     //--------------------
 
-    public function test_download_streaming()
+    public function cases_download_streaming_CSV_XLSX()
     {
-        $filename = "abc.csv";
+        return [
+            [true, "abc.csv"],
+            [false, "abc.xlsx"],
+        ];
+    }
 
+    /**
+     * @dataProvider cases_download_streaming_CSV_XLSX
+     * @param bool $iSCsv
+     * @param string $filename
+     */
+    public function test_download_streaming($iSCsv, $filename)
+    {
         $this->__mock_OnWeb();
         $this->onWeb->expects($this->once())->method('downloadHeader')->with($filename);
 
         ob_start();
-        $this->download_streaming($filename, ["NUM", "ABC"], [["1", "a"], ["2", "b"], ["3", "c"]]);
+        $this->download_streaming($iSCsv, $filename, ["NUM", "ABC"], [["1", "a"], ["2", "b"], ["3", "c"]]);
         $actual = ob_get_contents();
         ob_end_clean();
-        $this->assertEquals("NUM,ABC\n1,a\n2,b\n3,c\n", $actual);
+
+        if ($iSCsv) {
+            $this->assertEquals("NUM,ABC\n1,a\n2,b\n3,c\n", $actual);
+        } else {
+            $actual = $this->__extractDataFromXlsxBinary($actual);
+            $this->assertEquals([["NUM", "ABC"], ["1", "a"], ["2", "b"], ["3", "c"]], $actual);
+        }
     }
 
-    private function download_streaming($filename, $columns, $data)
+    private function download_streaming($iSCsv, $filename, $columns, $data)
     {
         $this->onWeb->downloadHeader($filename); //ここでヘッダ送出（ただしファイルサイズ不明）
-        $out = (new CsvFileWriterFlow())
-            ->CSV()
-            ->UTF8()
-            ->withoutBOM()
-            ->LF()
-            ->WITH_BR_at_EOF()
-            ->columns($columns)
-            ->toDownloadStreming()
-            ->begin();
+        if ($iSCsv) {
+            $out = (new CsvFileWriterFlow())
+                ->CSV()
+                ->UTF8()
+                ->withoutBOM()
+                ->LF()
+                ->WITH_BR_at_EOF()
+                ->columns($columns)
+                ->toDownloadStreming()
+                ->begin();
+        } else {
+            $out = (new CsvFileWriterFlow())
+                ->XLSX()
+                ->columns($columns)
+                ->toDownloadStreming()
+                ->begin();
+        }
         $this->__selectFromTable(
             $data,
             function ($line) use ($out) {
@@ -147,14 +202,12 @@ class CsvWriterControllerTest extends PHPUnit_Framework_TestCase
 
     //--------------------
 
-    public function test_download_after_making()
+    public function test_download_after_making_CSV()
     {
-        $path = FileFabricate::fromString("ID")->changeFileNameTo("abc.csv")->getPath();
-        unlink($path); //ファイルパスだけ取得したら、消しておく
-
+        $path = $this->__testPath('.csv');
+        $filename = "abc.csv";
         $columns = ["NUM", "ABC"];
         $data = [["1", "a"], ["2", "b"], ["3", "c"], ["4", "d"], ["5", "e"], ["6", "f"], ["7", "g"], ["8", "h"]];
-        $filename = "abc.csv";
 
         //初回アクセス
         $this->__mock_OnWeb();
@@ -163,7 +216,7 @@ class CsvWriterControllerTest extends PHPUnit_Framework_TestCase
             'isFinished' => false,
             'current' => 3,
         ]);
-        $this->download_after_making($path, $columns, $data, $filename);
+        $this->download_after_making('CSV', $path, $columns, $data, $filename);
         $this->assertEquals("NUM,ABC\n1,a\n2,b\n3,c\n", file_get_contents($path));
 
         //2回目アクセス
@@ -173,7 +226,7 @@ class CsvWriterControllerTest extends PHPUnit_Framework_TestCase
             'isFinished' => false,
             'current' => 6,
         ]);
-        $this->download_after_making($path, $columns, $data, $filename);
+        $this->download_after_making('CSV', $path, $columns, $data, $filename);
         $this->assertEquals("NUM,ABC\n1,a\n2,b\n3,c\n4,d\n5,e\n6,f\n", file_get_contents($path));
 
         //3回目アクセス
@@ -183,7 +236,7 @@ class CsvWriterControllerTest extends PHPUnit_Framework_TestCase
             'isFinished' => true,
             'current' => 8,
         ]);
-        $this->download_after_making($path, $columns, $data, $filename);
+        $this->download_after_making('CSV', $path, $columns, $data, $filename);
         $this->assertEquals("NUM,ABC\n1,a\n2,b\n3,c\n4,d\n5,e\n6,f\n7,g\n8,h\n", file_get_contents($path));
 
         //4回目アクセス
@@ -191,13 +244,46 @@ class CsvWriterControllerTest extends PHPUnit_Framework_TestCase
         $this->onWeb->expects($this->never())->method('ajaxHeader');
         $this->onWeb->expects($this->once())->method('downloadHeader')->with($path, $filename);
         ob_start();
-        $this->download_after_making($path, $columns, $data, $filename);
+        $this->download_after_making('CSV', $path, $columns, $data, $filename);
         $actual = ob_get_contents();
         ob_end_clean();
         $this->assertEquals("NUM,ABC\n1,a\n2,b\n3,c\n4,d\n5,e\n6,f\n7,g\n8,h\n", $actual);
     }
 
-    public function download_after_making($path, $columns, $data, $filename)
+    public function test_download_after_making_XLSX()
+    {
+        $path = $this->__testPath('.xlsx');
+        $filename = "abc.xlsx";
+        $columns = ["NUM", "ABC"];
+        $data = [["1", "a"], ["2", "b"], ["3", "c"], ["4", "d"], ["5", "e"], ["6", "f"], ["7", "g"], ["8", "h"]];
+
+        //初回アクセス
+        $this->__mock_OnWeb();
+        $this->onWeb->expects($this->once())->method('ajaxHeader');
+        $this->onWeb->expects($this->once())->method('ajaxBody')->with([
+            'isFinished' => true,
+            'current' => 8,
+        ]);
+        $this->download_after_making('XLSX', $path, $columns, $data, $filename);
+        $this->assertStringEndsWith(".xlsx", $path);
+        $this->assertFileExists($path);
+        $this->assertNotEquals(0, filesize($path));
+        $actual = (new CsvFileIterator($path))->toArrayWithColumns();
+        $this->assertEquals(array_merge([$columns], $data), $actual, "Excelファイルの内容が正しいこと");
+        $xlsxBinary = file_get_contents($path);
+
+        //2回目アクセス
+        $this->__mock_OnWeb();
+        $this->onWeb->expects($this->never())->method('ajaxHeader');
+        $this->onWeb->expects($this->once())->method('downloadHeader')->with($path, $filename);
+        ob_start();
+        $this->download_after_making('XLSX', $path, $columns, $data, $filename);
+        $actual = ob_get_contents();
+        ob_end_clean();
+        $this->assertEquals($xlsxBinary, $actual, sprintf("- %s\n+ %s", bin2hex($xlsxBinary), bin2hex($actual)));
+    }
+
+    public function download_after_making($csv_or_xlsx, $path, $columns, $data, $filename)
     {
         //ファイルゴミが残る可能性あるので注意
         /** @var WritingPosition $position */
@@ -206,21 +292,31 @@ class CsvWriterControllerTest extends PHPUnit_Framework_TestCase
             $position = new WritingPosition($path);
         }
         if ($position->isMaking()) {
-            $out = (new CsvFileWriterFlow())
-                ->CSV()
-                ->SJIS()
-                ->LF()
-                ->WITH_BR_at_EOF()
-                ->columns($columns)
-                ->toDownloadAfterMaking($position)
-                ->begin();
+            if ($csv_or_xlsx === 'CSV') {
+                $out = (new CsvFileWriterFlow())
+                    ->CSV()
+                    ->SJIS()
+                    ->LF()
+                    ->WITH_BR_at_EOF()
+                    ->columns($columns)
+                    ->toDownloadAfterMaking($position)
+                    ->begin();
+                $limit = self::RECORD_LIMIT;
+            } else {
+                $out = (new CsvFileWriterFlow())
+                    ->XLSX()
+                    ->columns($columns)
+                    ->toDownloadAfterMaking($position)
+                    ->begin();
+                $limit = null; //XLSX ではファイル追記はできないので分割しない（１度に全部吐く）
+            }
             $isFinished = $this->__selectFromTable(
                 $data,
                 function ($line) use ($out) {
                     $out->outputLine($line);
                 },
                 $position->getNextRownum() - 1,
-                self::RECORD_LIMIT
+                $limit
             );
             $out->finish($isFinished); //完了していたら$positionにフラグ立て
             $this->__session_set("position", $position);
@@ -276,5 +372,22 @@ class CsvWriterControllerTest extends PHPUnit_Framework_TestCase
             $onRead($line); //CsvWriter に渡す
         }
         return $isFinished;
+    }
+
+    private function __testPath($ext = "")
+    {
+        $path = tempnam('/tmp', 'test');
+        unlink($path);
+        $path = $path . $ext;
+        $this->paths[] = $path;
+        return $path;
+    }
+
+    private function __extractDataFromXlsxBinary($binary)
+    {
+        $path = $this->__testPath('.xlsx');
+        file_put_contents($path, $binary);
+        $data = (new CsvFileIterator($path))->toArrayWithColumns();
+        return $data;
     }
 }
